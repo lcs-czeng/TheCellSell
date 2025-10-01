9667d0af6781fe42d9cbc73cf3305912ae57cd34
Courage Zeng <czeng@lcs.on.ca>
2025-09-30 20:02:37 -0400

    Calculated and displayed cost for Plan A for Sample 1

--- TheCellSell/main.swift
+++ TheCellSell/main.swift
@@ -10,7 +10,10 @@ import Foundation
 // 1. Input
  
 // Get number of daytime minutes
-var dayTimeMinutes = 0
+var dayTimeMinutes = 251
+var nightTimeMinutes = 10
+var weekendTimeMinutes = 60
+
 while true {
     
     // Prompt
@@ -33,6 +36,52 @@ while true {
     break
  
 }
+
+while true {
+    
+    // Prompt
+    print("Number of nighttime minutes?")
+    
+    // Collect input
+    guard let givenInput = readLine() else {
+        // Repeat prompt, no input given
+        continue
+    }
+    
+    // Convert to integer
+    guard let givenInteger = Int(givenInput) else {
+        // Repeat prompt, not an integer
+        continue
+    }
+    
+    // Now we have an integer, break input loop
+    nightTimeMinutes = givenInteger
+    break
+ 
+}
+
+while true {
+    
+    // Prompt
+    print("Number of weekend minutes?")
+    
+    // Collect input
+    guard let givenInput = readLine() else {
+        // Repeat prompt, no input given
+        continue
+    }
+    
+    // Convert to integer
+    guard let givenInteger = Int(givenInput) else {
+        // Repeat prompt, not an integer
+        continue
+    }
+    
+    // Now we have an integer, break input loop
+    weekendTimeMinutes = givenInteger
+    break
+ 
+}
  
 // 2. Process
  
@@ -41,9 +90,13 @@ var a = 0
  
 // Add daytime cost
 a += (dayTimeMinutes - 100) * 25
- 
+a += (nightTimeMinutes * 15)
+a += (weekendTimeMinutes * 20)
+
+let resultA = Double(a) / 100
+
 // Calculate costs for plan B
  
 // 3. Output
-print("Plan A costs \(a)")
+print("Plan A costs \(resultA)")
  
